e3dfb538a46497b4e37e76a37e255f6a9ca89342
gzotpa <707185547@qq.com>
2023-03-24 19:11:35 +0800

    add attribute for component

--- lib/HEC/HECGenPass.cpp
+++ lib/HEC/HECGenPass.cpp
@@ -845,6 +845,7 @@ namespace mlir {
 
                 component = rewriter.create<mlir::hec::ComponentOp>(design.getLoc(), name,
                                                                     ports, interfc, style);
+                component->setAttr("dump", design->getAttr("dump"));
 
                 switch (this->style) {
                     case Style::NORMAL:
